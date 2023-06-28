407c2a7c841027ca63426a65ddb1cea8a852e0ba
matheus silva de souza <matheus.silva.ap14@gmail.com>
2023-06-28 17:31:52 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,5 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 
 def home(request):
-    return render(request, 'recipe/home.html')
-
-
-def contato(request):
-    return HttpResponse('contato3')
-
-
-def sobre(request):
-    return HttpResponse('sobre4')
+    return render(request, "recipe/home.html")
