c48d1dac3146f9ede0136f37a5b039f7717e2449
matheus silva de souza <matheus.silva.ap14@gmail.com>
2023-07-12 22:10:13 -0300

    add urls test

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, "recipes/pages/home.html", context={
-        'name': 'Matheus silva' 
+        'recipes': [make_recipe()for _ in range(10)],
+        'is_detail': True,
     })
 
 
 def recipe(request, id):
     return render(request, "recipes/pages/recipe-view.html", context={
-        'name': 'Matheus silva' 
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
